15010617feb024cf3312027746b3fa0139115b49
Anway Kapoor <anwaykapoor2311@gmail.com>
2024-04-27 19:56:15 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getApiConfiguration, getGenres } from './store/homeSlice';
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
-import {PageNotFound} from "./pages/404/PageNotFound"
+import PageNotFound from "./pages/404/PageNotFound"
 import Explore from "./pages/explore/Explore"
 import Details from "./pages/details/Details"
 import {Home} from "./pages/home/Home"
